8b9050d8bcf2325736a95b0d203562a3a7e56478
Eirik Baug <eirikbaug@hotmail.com>
2020-09-29 22:56:13 +0200

    initial autopilot

---
+++ src/autopilot.go
@@ -0,0 +1,30 @@
+package main
+
+func autopilot() error {
+	xH0, yH0, _, _, err := gui.ViewPosition(snekHead.viewName)
+	if err != nil {
+		return err
+	}
+	xB0, yB0, _, _, err := gui.ViewPosition(boxViewName)
+	if err != nil {
+		return err
+	}
+
+	if xH0 < xB0 && directionIsValid(directions.right) {
+		headDirection = directions.right
+	} else if xH0 > xB0 && directionIsValid(directions.left) {
+		headDirection = directions.left
+	} else if yH0 < yB0 && directionIsValid(directions.down) {
+		headDirection = directions.down
+	} else if yH0 > yB0 && directionIsValid(directions.up) {
+		headDirection = directions.up
+	}
+	return nil
+}
+
+func directionIsValid(direction direction) bool {
+	if getOppositeDirection(snekHead.currentDirection) == direction {
+		return false
+	}
+	return true
+}
