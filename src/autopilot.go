d358b7e47b4077323684bf2ccbf75669adf5683b
Eirik Baug <eirikbaug@hotmail.com>
2020-10-06 21:44:42 +0200

    starting a star implementation

--- src/autopilot.go
+++ src/autopilot.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"math"
-)
-
 type node struct {
 	direction direction
 	position  position
@@ -261,13 +257,6 @@ func autopilot2() error  {
 	}
 
 	return nil
-}
-
-func calculatePositionDistance(position1 position, position2 position) int  {
-	position1Col, position1Row := position1.x0/deltaX, position1.y0/deltaY
-	position2Col, position2Row := position2.x0/deltaX, position2.y0/deltaY
-
-	return int(math.Abs(float64(position1Col-position2Col)) + math.Abs(float64(position1Row - position2Row)))
 }
 
 func getPositionOfDirection(currentDirection direction, currentPosition position, positionMatrix [][]position) []node  {
