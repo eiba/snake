b333a6163a95445bf469f6289a37c25b10e3217e
Eirik Baug <eirikbaug@hotmail.com>
2020-10-07 21:57:59 +0200

    implementing priority queue

---
+++ src/priority_queue.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"container/heap"
+)
+
+type PriorityNode struct {
+	value    position
+	priority int
+	index    int
+}
+
+type PriorityQueue []*PriorityNode
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*PriorityNode)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) update(item *PriorityNode, value position, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
