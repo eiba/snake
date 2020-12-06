c589850902be7c4387738044448436645e292bfd
Eirik Baug <eirikbaug@hotmail.com>
2020-12-06 23:45:33 +0100

    remove unused autopilot function

--- src/autopilot2.go
+++ src/autopilot2.go
@@ -5,21 +5,6 @@ var (
 	pathIndex = -1
 )
 
-func autopilot3() error {
-	if pathIndex == len(foodPath)-1 {
-		pathIndex = 0
-		snekBodyPartSet := getSnekPositionSet(snekBodyParts)
-		foodPath = aStar(snekHead.position, foodView.position, snekBodyPartSet, positionMatrix)
-	}
-	if len(foodPath) == 0 {
-		headDirection = getRandomValidDirection(snekHead.currentDirection)
-		return nil
-	}
-	headDirection = foodPath[pathIndex].direction
-	pathIndex++
-	return nil
-}
-
 func initiateAStar(goal position) []node {
 	foodPath = aStar(snekHead.position, goal, getSnekPositionSet(snekBodyParts), positionMatrix)
 	if len(foodPath) == 0 {
@@ -62,30 +47,5 @@ func autopilot4() error {
 			headDirection = getRandomValidDirection(snekHead.currentDirection)
 		}
 	}
-	/*
-		headPosition := snekHead.position
-		headCycleIndex := cycleIndexMap[headPosition]
-		headCycleNode := hCycle[headCycleIndex]
-		if headCycleNode.direction != getOppositeDirection(snekHead.currentDirection) {
-			headDirection = headCycleNode.direction
-		}
-		foodPosition := foodView.position
-		foodCycleIndex := cycleIndexMap[foodPosition]
-		tailPosition := snekBodyParts[len(snekBodyParts)-1].position
-		tailCycleIndex := cycleIndexMap[tailPosition]
-		validNextPositions := getPositionOfDirection(snekHead.currentDirection, snekHead.position, positionMatrix)*/
-
-	/*for _, nextPosition := range validNextPositions {
-		nextPositionCycleIndex := cycleIndexMap[nextPosition.position]
-		//bestPathToFoodLength := math.MaxInt32
-		if nextPositionCycleIndex > headCycleIndex && nextPositionCycleIndex > tailCycleIndex && nextPositionCycleIndex < foodCycleIndex {
-			initiateAStar(foodView.position)
-			/*pathToFood := aStar(nextPosition.position, foodView.position, getSnekPositionSet(snekBodyParts), positionMatrix)
-			if  len(pathToFood) < bestPathToFoodLength {
-				headDirection = nextPosition.direction
-				bestPathToFoodLength = len(pathToFood)
-			}
-		}
-	}*/
 	return nil
 }
