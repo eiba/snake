e04768259559c9e41cc930cd2de7929944f478ed
Eirik Baug <eirikbaug@hotmail.com>
2020-12-13 00:06:42 +0100

    positionmatrix fix

--- game/position_matrix.go
+++ game/position_matrix.go
@@ -1,27 +1,25 @@
 package game
 
-import "github.com/eiba/snake"
-
-func initPositionMatrix(gameViewPosition position) {
-	gameViewCols := gameViewPosition.x1 / DeltaX
-	gameViewRows := gameViewPosition.y1 / DeltaY
-	if len(main.positionMatrix) == gameViewCols && len(main.positionMatrix[0]) == gameViewRows {
-		return
+func initPositionMatrix(gameViewPosition Position, positionMatrix [][]Position) [][]Position {
+	gameViewCols := gameViewPosition.X1 / DeltaX
+	gameViewRows := gameViewPosition.Y1 / DeltaY
+	if len(positionMatrix) == gameViewCols && len(positionMatrix[0]) == gameViewRows {
+		return positionMatrix
 	}
-	main.positionMatrix = generatePositionMatrix(gameViewPosition)
+	return generatePositionMatrix(gameViewPosition)
 }
 
-func generatePositionMatrix(gameViewPosition position) [][]position {
-	totalCols := gameViewPosition.x1 / DeltaX
-	totalRows := gameViewPosition.y1 / DeltaY
-	positionMatrix := make([][]position, totalCols)
+func generatePositionMatrix(gameViewPosition Position) [][]Position {
+	totalCols := gameViewPosition.X1 / DeltaX
+	totalRows := gameViewPosition.Y1 / DeltaY
+	positionMatrix := make([][]Position, totalCols)
 
 	for col := range positionMatrix {
-		positionMatrix[col] = make([]position, totalRows)
+		positionMatrix[col] = make([]Position, totalRows)
 		for row := range positionMatrix[col] {
 			x0 := col * DeltaX
 			y0 := row * DeltaY
-			position := position{x0, y0, x0 + DeltaX, y0 + DeltaY}
+			position := Position{x0, y0, x0 + DeltaX, y0 + DeltaY}
 			positionMatrix[col][row] = position
 		}
 	}
